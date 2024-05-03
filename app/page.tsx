1a60d52d23444d33e6ff913173998d0821381ae8
mdb-wonil <wonil@mdbtech.co.kr>
2024-05-03 10:19:59 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-  <div className='flex items-center justify-center h-screen'>
+    <div className="flex items-center justify-center h-screen">
       MDB FE Coding Test.
     </div>
-  );
+  )
 }
